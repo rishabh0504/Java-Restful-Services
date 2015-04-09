25b5cc5c5c20b9a4c6a73e79d47647fee34b3573
rishabh0504 <Rishi@0504>
2015-04-10 00:17:45 +0530

    CRD

--- src/com/avalia/restservices/RestfulServices.java
+++ src/com/avalia/restservices/RestfulServices.java
@@ -1,53 +1,61 @@
 package com.avalia.restservices;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.avalia.database.DataBase;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.json.JSONException;
 
-@Path("/path")
+import com.avalia.todo.Todo;
+import com.avalia.todo.Todos;
+import com.google.gson.Gson;
+
+@Path("/todos")
 public class RestfulServices {
 
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public List<String> getResponce() {
-		DataBase db = new DataBase();
-		List<String> list = db.get();
-		return list;
+	@Produces(MediaType.APPLICATION_JSON)
+	public String get() throws JSONException {
+		List<Todo> list = Todos.todos.get();
+		String json = new Gson().toJson(list);
+		return json.toString();
 	}
 
 	@PUT
 	@Path("put")
-	@Produces(MediaType.TEXT_PLAIN)
-	public List<String> putResponce(String data) {
-		DataBase db = new DataBase();
-		List<String> list = db.put(data);
-		return list;
+	@Produces(MediaType.APPLICATION_JSON)
+	public String put(String newTodo) throws JSONException, JsonParseException,
+			JsonMappingException, IOException {
+		Todo todo = new Todo(newTodo);
+		todo.save();
+		return "All done";
+
 	}
 
 	@POST
 	@Path("post")
-	@Produces(MediaType.TEXT_PLAIN)
-	public List<String> postResponce(String oldData, String newData) {
-		DataBase db = new DataBase();
-		List<String> list = db.post(oldData, newData);
-		return list;
+	@Produces(MediaType.APPLICATION_JSON)
+	public String post(String oldData, String newData) {
+
+		return null;
 	}
 
 	@DELETE
-	@Path("delete/data")
-	@Produces(MediaType.TEXT_PLAIN)
-	public List<String> deleteResponce(String data) {
-		DataBase db = new DataBase();
-		List<String> list = db.delete(data);
-		return list;
-
+	@Path("delete/desc")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String delete(String data) throws UnknownHostException {
+		Todo todo = new Todo();
+		String responce = todo.delete(data);
+		return responce;
 	}
 }
